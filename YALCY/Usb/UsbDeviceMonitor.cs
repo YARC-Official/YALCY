0e07e1591c59293df8a4d1614f6c7d5c5d049667
The Fat Bastid <tfb@sof.ws>
2024-11-02 00:10:43 -0400

    Fixed OEM stage kit not working.

--- YALCY/Usb/UsbDeviceMonitor.cs
+++ YALCY/Usb/UsbDeviceMonitor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Avalonia.Threading;
 using HidSharp;
 using YALCY.Integrations.StageKit;
@@ -12,6 +13,7 @@ public class UsbDeviceMonitor
 {
     private static List<HidDevice> _connectedDevices = new();
     private DeviceList _list = DeviceList.Local;
+    private static List<int> _connectedControllerIndices = new(); // Store connected XInput controller indices
 
     public event Action<HidDevice> DeviceInserted;
     public event Action<HidDevice> DeviceRemoved;
@@ -32,6 +34,8 @@ public class UsbDeviceMonitor
         {
             AddToList(dev);
         }
+
+        UpdateConnectedXInputControllers(); // Detect connected XInput controllers
     }
 
     public void StopUsbDeviceMonitor()
@@ -39,6 +43,7 @@ public class UsbDeviceMonitor
         DeviceInserted -= MainWindowViewModel.OnDeviceInserted;
         DeviceRemoved -= MainWindowViewModel.OnDeviceRemoved;
         _connectedDevices.Clear();
+        _connectedControllerIndices.Clear(); // Clear XInput controllers
     }
 
     private void OnDeviceListChanged(DeviceListChangedEventArgs e)
@@ -52,47 +57,98 @@ public class UsbDeviceMonitor
         {
             AddToList(dev);
         }
+        UpdateConnectedXInputControllers(); // Update XInput controller list on device change
+    }
+
+    private void AddToList(HidDevice dev)
+    {
+        if ((dev.VendorID != 0x1209 || dev.ProductID != 0x2882 || dev.ReleaseNumberBcd != 0x0900) && (dev.VendorID != 0x0E6F || dev.ProductID != 0x0103)) return;
+        _connectedDevices.Add(dev);
+        DeviceInserted?.Invoke(dev);
     }
 
     public static void SendReport(StageKitTalker.CommandId commandId, byte parameter)
     {
         OnStageKitCommand?.Invoke(commandId, parameter);
 
+        foreach (var controllerIndex in _connectedControllerIndices) // Only vibrate connected controllers
+        {
+            SetXInputVibration(controllerIndex, parameter, (byte)commandId);
+        }
+
         foreach (var device in _connectedDevices)
         {
             byte[] report;
-            if (device.VendorID == 0x1209) //santroller
+            if (device.VendorID == 0x1209 && device.ProductID == 0x2882 && device.ReleaseNumberBcd == 0x0900) //santroller sage kit
             {
                 report = new byte[4];
                 report[0] = 0x01;
                 report[1] = 0x5A;
                 report[2] = parameter;
                 report[3] = (byte)commandId;
-            }
-            else //stage kit
-            {
-                report = new byte[8];
-                report[0] = 0x00;
-                report[1] = 0x08;
-                report[2] = 0x00;
-                report[3] = parameter;
-                report[4] = (byte)commandId;
-                report[5] = 0x00;
-                report[6] = 0x00;
-                report[7] = 0x00;
-            }
 
-            var stream = device.Open();
-            stream.Write(report);
-            stream.Close();
+                var stream = device.Open();
+                stream.Write(report);
+                stream.Close();
+            }
         }
     }
 
-    private void AddToList(HidDevice dev)
+    private void UpdateConnectedXInputControllers()
     {
-        if ((dev.VendorID != 0x1209 || dev.ProductID != 0x2882 || dev.ReleaseNumberBcd != 0x0900) &&
-            (dev.VendorID == 0x0E6F || dev.ProductID != 0x0103)) return;
-        _connectedDevices.Add(dev);
-        DeviceInserted?.Invoke(dev);
+        _connectedControllerIndices.Clear();
+
+        for (int i = 0; i < 4; i++) // Check up to 4 controllers
+        {
+            XINPUT_STATE state = new XINPUT_STATE();
+            int result = XInputGetState(i, ref state);
+
+            if (result == 0) // Controller is connected
+            {
+                _connectedControllerIndices.Add(i);
+            }
+        }
+    }
+
+    private static void SetXInputVibration(int controllerIndex, byte leftMotor, byte rightMotor)
+    {
+        XINPUT_VIBRATION vibration = new XINPUT_VIBRATION
+        {
+            wLeftMotorSpeed = (ushort)(leftMotor << 8), // Left-shift by 8 bits
+            wRightMotorSpeed = (ushort)(rightMotor << 8) // Left-shift by 8 bits
+        };
+        XInputSetState(controllerIndex, ref vibration);
+    }
+
+    [DllImport("XInput1_4.dll", EntryPoint = "XInputSetState")]
+    private static extern int XInputSetState(int dwUserIndex, ref XINPUT_VIBRATION pVibration);
+
+    [DllImport("XInput1_4.dll", EntryPoint = "XInputGetState")]
+    private static extern int XInputGetState(int dwUserIndex, ref XINPUT_STATE pState);
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct XINPUT_STATE
+    {
+        public uint dwPacketNumber;
+        public XINPUT_GAMEPAD Gamepad;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct XINPUT_GAMEPAD
+    {
+        public ushort wButtons;
+        public byte bLeftTrigger;
+        public byte bRightTrigger;
+        public short sThumbLX;
+        public short sThumbLY;
+        public short sThumbRX;
+        public short sThumbRY;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct XINPUT_VIBRATION
+    {
+        public ushort wLeftMotorSpeed;
+        public ushort wRightMotorSpeed;
     }
 }
